1a8aba0d9154d9dd2ae37849e53e044701d2fafd
Gabriel <geonelson06@gmail.com>
2025-10-16 00:56:01 -0400

    Completed 1-5

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -1,25 +1,47 @@
-const addToFrontOrBack = () => {
+const addToFrontOrBack = (arr, value, isFront) => {
+  if (isFront) {
+    arr.unshift(value);
+  } else {
+    arr.push(value);
+  }
+
 };
 
-const reverseString = () => {
+const reverseString = (str) => {
+  return str.split('').reverse().join('');
+
 };
 
-const newArrayFullOf = () => {
+const newArrayFullOf = (value, numOfValue) => {
+  return Array(numOfValue).fill(value);
+
 };
 
-const insertIntoMiddle = () => {
+const insertIntoMiddle = (arr, value) => {
+  const middleIndex = Math.floor(arr.length / 2);
+  arr.splice(middleIndex, 0, value);
+
 };
 
-const deleteFromMiddle = () => {
+const deleteFromMiddle = (arr) => {
+  const middleIndex = Math.floor(arr.length / 2);
+  arr.splice(middleIndex, 1);
+
 };
 
-const isRightIndex = () => {
+const isRightIndex = (arr, value, index) => {
+  return arr[index] === value;
+
 };
 
-const roundAllNumsDown = () => {
+const roundAllNumsDown = (arr) => {
+  return arr.map(num => Math.floor(num));
+
 };
 
-const getAllYCoordinates = () => {
+const getAllYCoordinates = (arrOfCoords) => {
+  return arrOfCoords.map(coord => coord[1]);
+
 };
 
 module.exports = {
--- src/modify.js
+++ src/modify.js
@@ -1,14 +1,9 @@
-const uppercaseAll = (word1, word2, word3) => {
-  return [
-    word1.toUpperCase(),
-    word2.toUpperCase(),
-    word3.toUpperCase(),
-  ];
+const uppercaseAll = (...words) => {
+  return words.map(word => word.toUpperCase());
 };
 
 const destructureCoordinates = (coordinates) => {
-  const x = coordinates[0];
-  const y = coordinates[1];
+  const [x, y] = coordinates;
   return `X is: ${x}, Y is: ${y}`; // no touching this line!
 };
 
